c388d11edc41f150a97ad9a322b243043379fcc7
trevor dewindt <trevor.dewindt@gmail.com>
2023-12-05 21:39:57 -0700

    add final code

--- server/app.py
+++ server/app.py
@@ -20,19 +20,43 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakeries.append(bakery.to_dict())
+    response = make_response (
+        bakeries,
+        200
+    )   
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first().to_dict()
+    response = make_response(
+        bakery,
+        200
+    )
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = []
+    for baked_good in BakedGood.query.order_by(BakedGood.price.desc()).all():
+        baked_goods.append(baked_good.to_dict())
+    response = make_response (
+        baked_goods,
+        200
+    )   
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first().to_dict()
+    response = make_response (
+        baked_good,
+        200
+    )   
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
